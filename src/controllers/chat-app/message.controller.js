ad23ca757867c33e31fe4fdcb1bde73d2f957793
bhatiaheman <bhatiaheman8@gmail.com>
2024-01-21 23:01:30 +0530

    Fix typo in chatMessageCommonAggregation function

--- src/controllers/chat-app/message.controller.js
+++ src/controllers/chat-app/message.controller.js
@@ -8,7 +8,7 @@ import {  ApiResponse } from "../../utils/ApiResponse.js";
 import { asyncHandler } from "../../utils/AsyncHandler.js";
 
 
-const chatMessageCommonAggregatioon = () => {
+const chatMessageCommonAggregation = () => {
 
     return [
         {
@@ -40,5 +40,59 @@ const chatMessageCommonAggregatioon = () => {
 
 const getAllMessage = asyncHandler( async() => {
 
+    // select messages by chatid
+    // check the message is there or not in Chat database by id
+    // if no chat found
+    // if the user is not authenticated then access denied
+    // if user authenticated then create a message pipeline to get all message
+    // match with chat id and sort by createdAt
+    // return the response
+
+
+    const {chatId} = req.params;
+
+    const selectedChat = await Chat.findById(chatId);
+
+    if(!selectedChat){
+        throw new ApiError(404, "Chat not found");
+    }
+
+    if(!selectedChat.participants.includes(req.user._id)){
+        throw new ApiError(403, "Access denied");
+    }
+
+    const messages = await Message.aggregate(
+        [
+            {
+                $match: {
+                    chat: Mongoose.Types.ObjectId(chatId)
+                },
+            
+            },
+
+            ...chatMessageCommonAggregation(),
+
+            {
+                $sort: {
+                    createdAt: -1
+                }
+            },
+        ]
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, messages || [],  "Get all messages successfully")
+    )
     
 })
+
+
+const sendMessage = asyncHandler(async(req, res) => {
+
+    // take the chat id and content 
+    // check if the content is not attached or empty
+    // check if there the chat exists of not by id
+    //  
+});
